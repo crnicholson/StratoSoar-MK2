23a68eaca9e3df925bd051a08b24989a9cec947f
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-02-29 07:49:02 -0500

    Update settings.h

--- Code/Autopilots/autopilotIMU/headers/settings.h
+++ Code/Autopilots/autopilotIMU/headers/settings.h
@@ -1,3 +1,21 @@
+/*
+settings.h, part of StratoSoar MK2, for an autonomous glider.
+Copyright (C) 2024 Charles Nicholson
+
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Pins.
 #define RX 10       // TX pin of the mcuConn for communicating with the SAMD.
 #define TX 9        // RX pin of the mcuConn for communicating with the SAMD.
