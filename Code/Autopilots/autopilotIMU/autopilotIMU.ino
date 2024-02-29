10e4dc125d3a10280d160fccb6a2b3048053a114
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-02-29 07:49:44 -0500

    Update autopilotIMU.ino

--- Code/Autopilots/autopilotIMU/autopilotIMU.ino
+++ Code/Autopilots/autopilotIMU/autopilotIMU.ino
@@ -1,10 +1,24 @@
 // WARNING:
 // This is still a work in progress! Code may not work!
 
-// Part of StratoSoar MK2.
-// autopilotIMU.ino.
-// Charles Nicholson, 2023.
-// Code for a low-power autonomous glider.
+/*
+autopilotIMU.ino, part of StratoSoar MK2, for an autonomous glider.
+Copyright (C) 2024 Charles Nicholson
+
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // https://github.com/crnicholson/StratoSoar-MK2/.
 
 // Original work done by S.J. Remington 3/2020.
