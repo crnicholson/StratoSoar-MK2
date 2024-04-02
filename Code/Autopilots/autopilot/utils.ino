7b4eaeefeca6d1a2c9e01bcf9ff4301ae63718b2
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-04-01 20:07:32 -0400

    Update utils

--- Code/Autopilots/autopilot/utils.ino
+++ Code/Autopilots/autopilot/utils.ino
@@ -1,5 +1,5 @@
 /*
-utils.cpp, part of StratoSoar MK2, for an autonomous glider.
+utils.ino, part of StratoSoar MK2, for an autonomous glider.
 Copyright (C) 2024 Charles Nicholson
 
 This program is free software: you can redistribute it and/or modify
